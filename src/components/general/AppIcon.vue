b672cc957be8d186d24d4d856e23b2293741324b
Gorlenko Alexander <goral92@inbox.ru>
2023-05-24 12:13:18 +0300

    feat: добавляет иконки и компонет для работы с ними

---
+++ src/components/general/AppIcon.vue
@@ -0,0 +1,21 @@
+<template>
+	<svg aria-hidden="true">
+		<use :href="symbolId" :fill="color" />
+	</svg>
+</template>
+
+<script lang="ts" setup>
+import { computed } from "vue"
+
+interface IProps {
+	prefix?: string
+	name: string
+	color?: string
+}
+const props = withDefaults(defineProps<IProps>(), {
+	prefix: "icon",
+	color: "#00000",
+})
+
+const symbolId = computed(() => `#${props.prefix}-${props.name}`)
+</script>
