397e0a91d2aeb73372f2e366a2d2f95a077f33da
Gorlenko Alexander <goral92@inbox.ru>
2023-05-24 13:09:17 +0300

    feat: добавляет компонент для работы с иконкой

--- src/components/general/AppIcon.vue
+++ src/components/general/AppIcon.vue
@@ -1,5 +1,5 @@
 <template>
-	<svg aria-hidden="true">
+	<svg aria-hidden="true" class="icon">
 		<use :href="symbolId" :fill="color" />
 	</svg>
 </template>
@@ -11,11 +11,24 @@ interface IProps {
 	prefix?: string
 	name: string
 	color?: string
+	width?: string
+	height?: string
 }
 const props = withDefaults(defineProps<IProps>(), {
 	prefix: "icon",
 	color: "#00000",
+	width: "20px",
+	height: "20px",
 })
 
 const symbolId = computed(() => `#${props.prefix}-${props.name}`)
 </script>
+
+<style lang="scss" scoped>
+.icon {
+	display: inline-block;
+	width: v-bind(width);
+	height: v-bind(height);
+	fill: currentColor;
+}
+</style>
