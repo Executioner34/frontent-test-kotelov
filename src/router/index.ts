dd287658dbff1c2e267e50adc67ef77c65c85df5
Gorlenko Alexander <goral92@inbox.ru>
2023-05-24 13:07:23 +0300

    feat: добавляет глобальные импорты стилей в проект

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router"
-import HomePage from "../pages/homePage.vue"
+import HomePage from "@/pages/homePage.vue"
 
 const routes = [{ path: "/", component: HomePage }]
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,13 @@ import vue from "@vitejs/plugin-vue"
 import { createSvgIconsPlugin } from "vite-plugin-svg-icons"
 import path from "path"
 export default defineConfig({
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: `@import "@/assets/scss/_mixins.scss"; @import "@/assets/scss/_variables.scss";`,
+			},
+		},
+	},
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "src"),
@@ -13,7 +20,7 @@ export default defineConfig({
 		createSvgIconsPlugin({
 			// Specify the icon folder to be cached
 			iconDirs: [path.resolve(process.cwd(), "src/assets/icons")],
-			symbolId: "icon-[dir]/[name]",
+			symbolId: "icon-[dir]-[name]",
 			inject: "body-last" || "body-first",
 			customDomId: "__svg__icons__dom__",
 		}),
